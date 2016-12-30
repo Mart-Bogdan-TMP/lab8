72a82b9d02a1e829cb6aeb0d95157b1da6c211ea
MasterOfTheU <leiaorgana228@gmail.com>
2016-12-30 15:38:57 +0200

    url changes

--- tables/urls.py
+++ tables/urls.py
@@ -4,16 +4,22 @@ from . import views
 app_name = 'tables'
 
 urlpatterns = [
+    #return to homepage
     url(r'^info/', views.to_homepage, name='homepage'),
-    # /tables/
+
+#=================================================================================
+#TABLES
+#=================================================================================
     url(r'^$', views.index, name='index'),
 
-     # /tables/table_name_details/
+    # /posts/
+
     url(r'^tables/posts', views.details_posts, name='details_posts'),
-
-    #add_objectname
     url(r'^post_create/$', views.PostCreate.as_view(), name='create_post'),
+    url(r'^post_delete/(?P<pk>\d+)$', views.post_delete, name='delete_post'),
 
-    # remove_objectname
-    url(r'^delete/(?P<pk>\d+)$', views.post_delete, name='delete_post'),
+    # /workers/
+    url(r'^tables/workers', views.details_workers, name='details_workers'),
+    url(r'^worker_create/$', views.WorkerCreate.as_view(), name='create_worker'),
+    url(r'^worker_delete/(?P<pk>\d+)$', views.worker_delete, name='delete_worker'),
 ]
